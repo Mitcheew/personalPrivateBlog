efb2d6d2268bcfdc784382b3c5706362d96da5d5
Mitcheew <wowmormon@gmail.com>
2019-10-22 15:11:22 -0600

    containing proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
-const proxy = require('http-proxy-middleware')
+// const proxy = require('http-proxy-middleware')
  
-module.exports = function(app) {
-  app.use(proxy('/api', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
-  app.use(proxy('/auth', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
-  app.use(proxy('/sign-s3', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
-}
+// module.exports = function(app) {
+//   app.use(proxy('/api', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
+//   app.use(proxy('/auth', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
+//   app.use(proxy('/sign-s3', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
+// }
