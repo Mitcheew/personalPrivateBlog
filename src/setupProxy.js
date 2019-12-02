ac14ec5781b7aac7d2fa81189e530a6ba7f99db0
Mitcheew <wowmormon@gmail.com>
2019-12-02 13:22:58 -0700

    changing setup proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware')
  
 module.exports = function(app) {
-  app.use(proxy('/api', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
-  app.use(proxy('/auth', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
-  app.use(proxy('/sign-s3', { target: `${process.env.REACT_APP}${process.env.REACT_APP_SERVER_PORT}/` }))
+  app.use(proxy('/api/**', { target: `${process.env.REACT_APP}/` }))
+  app.use(proxy('/auth/**', { target: `${process.env.REACT_APP}/` }))
+  app.use(proxy('/sign-s3/**', { target: `${process.env.REACT_APP}/` }))
 }
